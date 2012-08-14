7e23a50e58e183c75db486e3862d9adfedfe79d0
Omri Gazitt <ogazitt@gmail.com>
2012-08-14 12:52:29 -0700

    added gallery database and model files

---
+++ source/services/serverentities/GalleryActivity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuiltSteady.Product.ServerEntities
+{
+    public class GalleryActivity
+    {
+        [Key]
+        public string Name { get; set; }
+        public string Definition { get; set; }
+        public int CategoryID { get; set; }
+        public GalleryCategory Category { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
---
+++ source/services/serverentities/GalleryCategory.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuiltSteady.Product.ServerEntities
+{
+    public class GalleryCategory
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public int? ParentID { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
